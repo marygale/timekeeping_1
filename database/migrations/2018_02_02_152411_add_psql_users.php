729c4fc7565d79e38419c3113945ad57bdd193c6
angelobagasbas <angelo.bagasbas@gmail.com>
2018-02-02 23:43:09 +0800

    fix migrations

--- database/migrations/2018_02_02_152411_add_psql_users.php
+++ database/migrations/2018_02_02_152411_add_psql_users.php
@@ -20,26 +20,28 @@ class AddPsqlUsers extends Migration
         $user_admin_role = Role::where('name','admin')->first();
         $user_super_admin_role = Role::where('name','super_admin')->first();
 
-        $user_user = User::create([
-            'name' => 'Coco Martin',
-            'email' => 'user@email.com',
-            'password' => bcrypt('php123'),
-            'remember_token' => str_random(10),
-        ]);
+        $user_user = new User;
+        $user_user->name = 'Coco Martin';
+        $user_user->email = 'user@email.com';
+        $user_user->password = bcrypt('php123');
+        $user_user->remember_token = str_random(10);
+        $user_user->save();
 
-        $user_admin = User::create([
-            'name' => 'Dingdong Dantes',
-            'email' => 'admin@email.com',
-            'password' => bcrypt('php123'),
-            'remember_token' => str_random(10),
-        ]);
 
-        $user_super_admin = User::create([
-            'name' => 'Coco Martin',
-            'email' => 'super_admin@email.com',
-            'password' => bcrypt('php123'),
-            'remember_token' => str_random(10),
-        ]);
+        $user_admin = new User;
+        $user_admin->name = 'Dingdong Dantes';
+        $user_admin->email = 'admin@email.com';
+        $user_admin->password = bcrypt('php123');
+        $user_admin->remember_token = str_random(10);
+        $user_admin->save();
+
+
+        $user_super_admin = new User;
+        $user_super_admin->name = 'Piolo Pascual';
+        $user_super_admin->email = 'super_admin@email.com';
+        $user_super_admin->password = bcrypt('php123');
+        $user_super_admin->remember_token = str_random(10);
+        $user_super_admin->save();
 
         User::find($user_user)->_roles()->attach($user_role);
         User::find($user_admin)->_roles()->attach($user_admin_role);
